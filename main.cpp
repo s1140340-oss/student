f6d39feb886c76c564ed86ea3ecd80d15896fce8
s1140340-oss <s1140340@dmhs.kh.edu.tw>
2025-10-27 16:24:42 +0800

    hw2.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout<< "hello, world!" << std::endl;
+    int age = 9999999;
+    double height = 180;
+    std::string name = "王基佑";
+    bool is_student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << height << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_student << std::endl;
+    
+    
+    
+
+
     return 0;
 }
