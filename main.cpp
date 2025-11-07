a4198326f2c7fcaaa648aebff4c2f991801bf1c7
s1140340-oss <s1140340@dmhs.kh.edu.tw>
2025-11-07 11:58:49 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
-int main() {
-    int age = 9999999;
-    double height = 180;
-    std::string name = "王基佑";
-    bool is_student = false;
-    std::cout << "姓名: " << name << std::endl;
-    std::cout << "身高: " << height << std::endl;
-    std::cout << "年齡: " << age << std::endl;
-    std::cout << "是否為學生: " << is_student << std::endl;
+int main()
+{
     
-    
-    
-
+    std::cout<<"Iamgod" << std::endl;
+    std::cout<<"I am god" << std::endl;
+    std::cout<<"I\tam\tgod" << std::endl;
+    std::cout<<"I\nam\ngod" << std::endl;
+    std::cout<<"\"Iamgod\"" << std::endl;
+    std::cout<<"\\Iamgod\\" << std::endl;
 
     return 0;
 }
